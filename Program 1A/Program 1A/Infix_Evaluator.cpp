2e5810d5e77540456fa8ff51e36856a910140893
cderk6 <cderk6@mail.umkc.edu>
2016-09-22 17:08:01 -0500

    Infix evaluator stuff

--- Program 1A/Program 1A/Infix_Evaluator.cpp
+++ Program 1A/Program 1A/Infix_Evaluator.cpp
@@ -1,1 +1,24 @@
 #include "Infix_Evaluator.h"
+
+const string OPERATORS[] = { "||", "&&", "==", "!=", "<", ">", ">=", "<=", "+", "-", "*", "/" , "%", "^", "-", "--", "++", "!" };
+const int PRECEDENCE[] = { 1, 2, 3, 3, 4, 4, 4, 4, 5, 5, 6, 6, 6, 7, 8, 8, 8, 8 };
+
+int Infix_Evaluator::evaluate(string& expression)
+{
+	stack<int> operands;
+	stack<string> operators;
+	while (!expression.empty())
+	{
+		// String_Tokenizer tokenizer;
+		// string token = tokenizer.nextToken(expression);
+		// if (!token.isOperator())
+		//	
+
+	}
+}
+
+int Infix_Evaluator::isOperator(string op)
+{
+	for (int i = 0; i < 18; i++)
+
+}
--- Program 1A/Program 1A/Infix_Evaluator.h
+++ Program 1A/Program 1A/Infix_Evaluator.h
@@ -2,11 +2,18 @@
 #define INFIX_EVALUATOR_H
 
 #include <string>
+#include "String_Tokenizer.h"
+#include <stack>
 
 using namespace std;
 
 class Infix_Evaluator
 {
+private:
+	int isOperator(string op);
+
+public:
+	int evaluate(string& expression);
 
 };
 
